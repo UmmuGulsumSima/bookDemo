cf9fe990161b53a32fe14bda3937fa497285e010
UmmuGulsumSima <144490314+UmmuGulsumSima@users.noreply.github.com>
2024-09-25 12:23:20 +0300

    Proje dosyası ekle.

---
+++ bookDemo/Data/ApplicationContex.cs
@@ -0,0 +1,22 @@
+﻿namespace bookDemo.Data 
+{ 
+
+    using bookDemo.Models;
+
+
+    public static class ApplicationContex
+    {
+
+        public static List<Book> Books { get; set; }
+        static  ApplicationContex()
+        {
+        Books = new List<Book>()
+        {
+            new Book() { Id=1 , Title= "AhiEvran", Price=200},
+            new Book() { Id=2, Title= "Lal", Price =203},
+            new Book() { Id= 3,Title="Aşkın gözyaşları" ,Price=203}
+        };
+
+        }
+    }
+}
